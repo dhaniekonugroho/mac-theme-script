98cca2890e66e7688f32f1979ae563c9228feb3a
dhaniekonugroho <dhaniekonugroho@gmail.com>
2019-01-03 10:17:31 +0000

    update_01
    Change the download link to git

--- mac.sh
+++ mac.sh
@@ -12,10 +12,8 @@ mkdir ~/.icons
 mkdir ~/.themes
 #Download McOS Space-Grey theme and Mac OS Icon Theme
 cd /tmp
-wget https://github.com/vinceliuice/Mojave-gtk-theme/raw/images/MacOSX-icon-theme.tar.xz
-wget https://dl.opendesktop.org/api/files/download/id/1538007151/s/23addba08e7013981da01fdff724e556a717ec73a1287eebf3ac91bea2498f0d18f62991026c908abbb959700b887cd5c228345e6f6ca275c5253fb6e2b83c89/t/1546470178/u//McOS-Space-Grey-3.30.tar.xz
-#Download Mac MAJOVE Shell theme
-wget https://dl.opendesktop.org/api/files/download/id/1545322951/s/fbba5360ade9d2acee51b8b7f356e6ab12f7234f902384d2fa75666fd48435d4fc459c2a42080a450af042f30ed4d5f4548ecb0c9a7e0dc18cffae399819e243/t/1546471900/u//DARK.for.Dash.to.DOCK.tar.xz
+#cloning the git repository
+git clone https://github.com/dhaniekonugroho/mac-theme-script.git
 tar -xvf DARK.for.Dash.to.DOCK.tar.xz
 tar -xvf McOS-Space-Grey-3.30.tar.xz
 tar -xvf MacOSX-icon-theme.tar.xz
